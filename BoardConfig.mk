0ef9fa55d60cf73de76b85e9ab686f30d2345c76
Ziyann <jaraidaniel@gmail.com>
2014-09-09 10:18:06 +0200

    Use the generic defconfig
    Change-Id: I5a278f4e0c6501c36bf545125f4645bf883c9244

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -2,10 +2,10 @@
 include device/acer/t30-common/BoardConfigCommon.mk
 
 TARGET_BOOTLOADER_BOARD_NAME := picasso_m
-TARGET_OTA_ASSERT_DEVICE := picasso_m,a510_pa_cus1
+TARGET_OTA_ASSERT_DEVICE := picasso_m,a510_pa_cus
 
 # Kernel defconfig
-TARGET_KERNEL_CONFIG := cyanogenmod_picasso_m_defconfig
+TARGET_KERNEL_CONFIG := picasso_m_defconfig
 
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/acer/a510/bluetooth
