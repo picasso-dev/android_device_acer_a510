27c76584328479cc403852c3f3c2448ed5d6c063
Shreps <shr3ps@gmail.com>
2015-06-06 17:52:17 +0200

    Using Z-kernel defconfig

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,10 +5,11 @@ TARGET_BOOTLOADER_BOARD_NAME := picasso_m
 TARGET_OTA_ASSERT_DEVICE := picasso_m,a510_pa_cus1
 
 # Kernel defconfig
-TARGET_KERNEL_CONFIG := cyanogenmod_picasso_m_defconfig
+#TARGET_KERNEL_CONFIG := cyanogenmod_picasso_m_defconfig
+TARGET_KERNEL_CONFIG := picasso_m_defconfig
 
 # Bluetooth
-#BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/acer/a510/bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/acer/a510/bluetooth
 
 # Recovery
 TARGET_RECOVERY_FSTAB := device/acer/a510/prebuilt/ramdisk/fstab.acer
